da7697c6f2217afe4d7cb1cc8b192738da525c94
pang <695441271@qq.com>
2023-09-15 09:01:25 +0800

    three commit

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git demo 1");
         System.out.println("git demo 2");
+        System.out.println("git demo 3");
     }
 }
