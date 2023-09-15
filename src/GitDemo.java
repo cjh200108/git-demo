751c9af52e816e9ddcb1e88ea847f941d7cd815c
pang <695441271@qq.com>
2023-09-15 08:57:45 +0800

    first commit

---
+++ src/GitDemo.java
@@ -0,0 +1,11 @@
+/**
+ * @author pang
+ * @version 1.0
+ * @date 2023/9/15 8:56
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("git demo 1");
+    }
+}
